dc0f5303cce9340949b9708ab2139294d8c9816c
Wendell <djwendell@gmail.com>
2025-12-15 13:39:48 -0300

    Configuração Tailwind, PostCSS e PWA

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
